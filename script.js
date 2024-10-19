bd1f7b35f457ca6876e6f3cb69d722754515c56d
phamdat1234 <23130056@st.hcmuaf.edu.vn>
2024-10-19 17:13:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-    
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none"; // Ẩn tất cả tab
+        tabcontent[i].style.display = "none";
     }
 
     tablinks = document.getElementsByClassName("tablinks");
@@ -11,6 +11,6 @@ function openTab(evt, tabName) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    document.getElementById(tabName).style.display = "block"; // Hiển thị tab hiện tại
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
